0b5f4bed5a3a0191f5ae468d01b51366687a779f
Cristian Sitov <cristian.sitov@e-spres-oh.com>
2017-08-08 12:35:25 +0300

    Cleanup a few things

--- resources/views/backend/heritage/includes/header-buttons.blade.php
+++ resources/views/backend/heritage/includes/header-buttons.blade.php
@@ -5,15 +5,13 @@
 
 <div class="pull-right mb-10 hidden-lg hidden-md">
     <div class="btn-group">
-        <button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-            {{ trans('menus.backend.heritage.resources.main') }} <span class="caret"></span>
-        </button>
+        {{--<button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown" aria-expanded="false">--}}
+            {{--{{ trans('menus.backend.heritage.resources.main') }} <span class="caret"></span>--}}
+        {{--</button>--}}
 
         <ul class="dropdown-menu" role="menu">
             <li>{{ link_to_route('admin.heritage.resource.index', trans('menus.backend.heritage.resources.all')) }}</li>
-            @if (access()->hasRoles(['Administrator', 1]))
             <li>{{ link_to_route('admin.heritage.resource.create', trans('menus.backend.heritage.resources.create')) }}</li>
-            @endif
         </ul>
     </div><!--btn group-->
 </div><!--pull right-->
